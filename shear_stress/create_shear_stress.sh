45ef5428269a0badb34ecb7f4304ca47c6783348
Evan Gowan <evangowan@gmail.com>
2024-07-19 18:47:18 +0900

    added shear stress calcualted from Bedmachine ice thickness data

--- shear_stress/create_shear_stress.sh
+++ shear_stress/create_shear_stress.sh
@@ -42,3 +42,42 @@ gmt begin shear_stress pdf
   gmt colorbar -DJBC+w7c/0.5c+h+   -Bx100000f20000+l"Basal Shear Stress (Pa)"  -Cshades_shearstress.cpt
 
 gmt end
+
+# create shear stress from Bedmachine values
+
+bedmachine=../topo/bedmachine_shear_stress.nc
+bedmachine_smooth=../topo/smoothed_bedmachine_shear_stress.nc
+
+gmt grdmath ${bedmachine} ${shear_stress_grid} DENAN = bedmachine_shear_stress.nc
+
+
+
+bin_file="bedmachine_shear_stress.bin"
+
+gmt grdconvert bedmachine_shear_stress.nc ${bin_file}=bf 
+
+param_file=bedmachine_ss_parameters.txt
+
+echo ${bin_file} > ${param_file}
+echo ${x_min} >> ${param_file}
+echo ${x_max} >> ${param_file}
+echo ${y_min} >> ${param_file}
+echo ${y_max} >> ${param_file}
+echo ${resolution}000 >> ${param_file}
+
+
+
+
+
+gmt begin bedmachine_shear_stress pdf
+
+  gmt grdimage bedmachine_shear_stress.nc   -Cshades_shearstress.cpt -Q   ${J_cart} ${R_cart}
+  gmt plot shear_stress_domains.shp ${J_cart} ${R_cart}  -Wthin,grey -BneWS
+  gmt plot ../coastline/coastline.shp ${R_options} ${J_options}  -Wthin -BneWS
+  gmt plot ../ice_margins/modern.shp ${R_options} ${J_options}  -Wthick,blue -BneWS
+  gmt basemap  ${R_options} ${J_options} -Ln0.8/0.04+w500+c-43/73+f+l+ar -F+gwhite+p2p,black
+  gmt colorbar -DJBC+w7c/0.5c+h+   -Bx100000f20000+l"Basal Shear Stress (Pa)"  -Cshades_shearstress.cpt
+
+gmt end
+
+
